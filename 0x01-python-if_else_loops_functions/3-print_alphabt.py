c6e477be015931f128d5689074ba20000a17aa9f
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-06-08 02:07:45 +0100

    use print function with string format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for the_alphabet in range(ord('a'), ord('z')+1):
     if chr(the_alphabet) not in ['q', 'e']:
-        print(chr(the_alphabet), end='')
+        print("{}".format(chr(the_alphabet)), end='')
