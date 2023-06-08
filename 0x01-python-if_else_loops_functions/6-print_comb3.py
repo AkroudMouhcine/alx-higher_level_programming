8d3d0493da65216f8f01cbc49a63d3a55f9af9a0
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-06-08 03:25:56 +0100

    fix py_code_style

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for number_1 in range(10):
-    for number_2 in range(number_1 + 1, 10):
-        print("{:d}{:d}".format(number_1, number_2), end=", " if number_1 < 8 else "\n" )
+for num_1 in range(10):
+    for num_2 in range(num_1 + 1, 10):
+        print("{:d}{:d}".format(num_1, num_2), end=", " if num_1 < 8 else "\n")
