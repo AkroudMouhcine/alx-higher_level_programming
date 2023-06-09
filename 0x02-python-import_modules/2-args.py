7878e48c0360b2708c4f2e6d7399dbcfeca3282f
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-06-09 03:08:58 +0100

    Fix the code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-
-def count_arguments(args):
-    num_args = len(args)
-    return num_args
-
-
-def print_arguments(args):
-    for index, arg in enumerate(args):
-        print(f"{index}: {arg}")
-
-
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    num_args = count_arguments(args)
-    print(f"{num_args} argument{'s' if num_args != 1 else ''}\
-{'.' if num_args == 0 else ':'}")
-    print_arguments(args)
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for index, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{index}: {arg}")
