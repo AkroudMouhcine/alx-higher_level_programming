b8c06670116c96956baaef7b9cdc62d78b05c678
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-06-14 23:11:21 +0100

    fix pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[row[element_index]**2 for element_index in range(len(row))] for row in matrix] 
+    return [[row[element]**2 for element in range(len(row))] for row in matrix]
